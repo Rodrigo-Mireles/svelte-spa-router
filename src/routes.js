d4c8e2c676762c1218edc6a01e3e87a21f47485f
Rodrigo Mireles <rodrigomireles2001@gmail.com>
2022-01-23 22:16:14 -0600

    nuevo compenente nosotros

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,12 @@
 import Inicio from './Principal/Inicio.svelte'
 import Error  from './Principal/Error.svelte'
 import Contacto from './Principal/Contacto.svelte'
+import Nosotros from './Principal/Nosotros.svelte'
 
 const routes = {
     '/':Inicio,
     '/contacto':Contacto,  
+    '/nosotros':Nosotros,
     '*':Error
      
 }
