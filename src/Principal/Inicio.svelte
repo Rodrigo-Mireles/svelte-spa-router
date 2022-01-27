d3dca3c8b23746a930776527d242d7c36b1659fe
Rodrigo Mireles <rodrigomireles2001@gmail.com>
2022-01-26 18:06:53 -0600

    Inicie con el diseño de pagina ENCO

--- src/Principal/Inicio.svelte
+++ src/Principal/Inicio.svelte
@@ -1,2 +1,36 @@
-<h1>Inicio</h1>
-
+<!-- Header -->
+<div id="primario" class="container-fluid pb-3 pt-3">
+  <div class="row text-end">
+    <div class="col align-self-end">
+      <a>Acompañar,contruir,actuar</a>
+    </div>
+  </div>
+</div>
+<nav id="header"class="navbar navbar-expand-lg navbar-ligth bg-ligth">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">
+        <img src="enco-img/logo-enco.png" alt=" ">
+        En Compañia Efectiva
+      </a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Nosotros</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Noticias</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Clientes</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Contacto</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+<!-- /Header -->
